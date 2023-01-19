2c2400c3a3aab5a120b043943b3e7778d2432d53
Arunchandra Boini <arunchandra.boini@effigo.in>
2023-01-19 12:25:52 +0530

    Updated pom build tag and version to 1.8

--- src/main/java/com/arun/training/Application.java
+++ src/main/java/com/arun/training/Application.java
@@ -1,11 +1,23 @@
 package main.java.com.arun.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
+        List<String> names = new ArrayList<String>();
+        names.add("arun");
+        names.add("mani");
+        names.add("appu");
+        names.forEach((name)->{
+            System.out.println(name);
+        });
+
     }
 
+    
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
